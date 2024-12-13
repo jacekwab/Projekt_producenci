87dd10daffa1f9099b15c235e8775c48a0c52593
jerrychutz <jeremiasz@chudzik.waw.pl>
2024-12-13 12:19:43 +0100

    Najprostsza wersja wyświetlania za pomocą Flask (#1)
    Dodaję najprostszą wersję wyświetlania danych we Flask, którą wzoruję na minimalistycznej, jedno-plikowej aplikacji opisanej pod następującym linkiem: https://flask.palletsprojects.com/en/stable/quickstart/ . Zdecydowałem, że na tym etapie podejście małych kroków będzie ok.
    Rozwinięcie do struktury bliższej docelowej byłoby przesadą w tym momencie, wymagałoby o wiele więcej czasu. Jak na pojedynczy komit jest tu dostatecznie dużo zmian.
    
    Z najważniejszych zmian: dodałem utworzenie obiektu aplikacji Flask, przekształciłem zwykłe printy na html, usunąłem globalne zmienne, zmieniłem strukturę kodu tak, że teraz deklaracje funkcji zostały wyniesione ponad przepływ, czyli nie są wtrącane w głównym przepływie a deklarowane na samym początku.

--- Projekt_Allegro/main.py
+++ Projekt_Allegro/main.py
@@ -1,103 +1,121 @@
 import requests
 from dotenv import load_dotenv
 import os
+from flask import Flask
 
-load_dotenv()
-# Dane do uwierzytelnienia
-#zmienne lokalne znajdują się w pliku .env
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+app = Flask(__name__)
 
-# Endpointy Allegro API
-AUTH_URL = "https://allegro.pl/auth/oauth/token"
-SEARCH_URL = "https://api.allegro.pl/offers/listing"
-PRODUCTS_URL = "https://api.allegro.pl/sale/products"
-file_token_path = "./token.txt"
+@app.route("/")
+def data_display():
 
 
-#Sandbox:
-# AUTH_URL = "https://allegro.pl.allegrosandbox.pl/auth/oauth/token"
-# SEARCH_URL = "https://api.allegro.pl.allegrosandbox.pl/offers/listing"
+    def data_downloading_and_formatting():
 
 
-def get_access_token(client_id, client_secret):
-    global token
-    """
-    Funkcja uzyskuje token dostępu do API Allegro.
-    """
-    response = requests.post(
-        AUTH_URL,
-        auth=(client_id, client_secret),
-        data={'grant_type': 'client_credentials'}
-    )
-    response.raise_for_status()
-    token = response.json()['access_token']
-    print(token)
-    with open(f"{file_token_path}", "w") as file_token:
-        file_token.write(token)
-    return token
+        def get_and_format_products(phrase, token):
+            PRODUCTS_URL = "https://api.allegro.pl/sale/products"
+            
+            # Parametry zapytania i nagłówki
+            headers = {
+                'Authorization': f'Bearer {token}',
+                'Accept': 'application/vnd.allegro.public.v1+json',
+            }
+            params = {
+                'phrase': phrase,
+            }
+
+            # Pobieranie produktów
+            response = requests.get(PRODUCTS_URL, headers=headers, params=params)
+            response.raise_for_status()
+            data = response.json()
+
+            # Przetwarzanie wyników
+            products = data.get('products', [])
+
+            # Wyświetlenie wyników
+            formatted_data = f"Liczba produktów dla frazy '{phrase}': {len(products)}"
+            for product in products:
+
+                formatted_data = formatted_data + "<br>" + f"Produkt ID: {product.get('id', 'Brak danych')}"
+                formatted_data = formatted_data + "<br>" + f"Nazwa: {product.get('name', 'Brak nazwy')}"
+
+                # Pobieramy nazwę producenta, jeśli jest dostępna
+                try:
+                    brand = product['parameters'][0]['valuesLabels'][0]
+                    formatted_data = formatted_data + "<br>" + f"Marka: {brand}"
+                except:
+                    formatted_data = formatted_data + "<br>" + "Producent: Brak danych"
+
+                # Pobieramy inne szczegóły, takie jak EAN, jeśli są dostępne
+                formatted_data = formatted_data + "<br>" + f"EAN: {product.get('ean', 'Brak danych')}"
+                # Nowa linia + separator dla czytelności
+                formatted_data = formatted_data + "<br>" + "-" * 40
+
+            formatted_data = f"<p>{formatted_data}</p>"
+
+            return formatted_data
 
 
-# Jeżeli plik token.txt istnieje. Pobiera się token z pliku.
-if os.path.exists(file_token_path) == False:
-    file_token = open(f"{file_token_path}", "w")
-    file_token.close()
-    token = ""
-else:
-    with open(f"{file_token_path}", "r") as file_token:
-        token = file_token.readline()
+
+        def get_access_token(client_id, client_secret):
+            """
+            Funkcja uzyskuje token dostępu do API Allegro.
+            """
+            response = requests.post(
+                AUTH_URL,
+                auth=(client_id, client_secret),
+                data={'grant_type': 'client_credentials'}
+            )
+            response.raise_for_status()
+            token = response.json()['access_token']
+
+            with open(f"{file_token_path}", "w") as file_token:
+                file_token.write(token)
+
+            return token
 
 
-def get_products(phrase, CLIENT_ID, CLIENT_SECRET):
-    PRODUCTS_URL = "https://api.allegro.pl/sale/products"
-    # Parametry zapytania i nagłówki
-    headers = {
-        'Authorization': f'Bearer {token}',
-        'Accept': 'application/vnd.allegro.public.v1+json',
-    }
-    params = {
-        'phrase': phrase,
-        'limit': 100  # Liczba produktów na stronę
-    }
 
-    # Pobieranie produktów
-    response = requests.get(PRODUCTS_URL, headers=headers, params=params)
-    response.raise_for_status()
-    data = response.json()
+        load_dotenv()
+        # Dane do uwierzytelnienia
+        #zmienne lokalne znajdują się w pliku .env
+        CLIENT_ID = os.getenv("CLIENT_ID")
+        CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
-    # Przetwarzanie wyników
-    products = data.get('products', [])
+        # Endpointy Allegro API
+        AUTH_URL = "https://allegro.pl/auth/oauth/token"
+        SEARCH_URL = "https://api.allegro.pl/offers/listing"
+        PRODUCTS_URL = "https://api.allegro.pl/sale/products"
+        file_token_path = "./token.txt"
 
-    # Wyświetlenie wyników
-    print(f"Liczba produktów dla frazy '{phrase}':", len(products))
-    for product in products:
 
-        print("Produkt ID:", product.get('id', 'Brak danych'))
-        print("Nazwa:", product.get('name', 'Brak nazwy'))
 
-        # Pobieramy nazwę producenta, jeśli jest dostępna
+        # Jeżeli plik token.txt istnieje. Pobiera się token z pliku.
+        if os.path.exists(file_token_path) == False:
+            file_token = open(f"{file_token_path}", "w")
+            file_token.close()
+            token = ""
+        else:
+            with open(f"{file_token_path}", "r") as file_token:
+                token = file_token.readline()
+
+        # Użycie funkcji (podaj poprawne client_id i client_secret dla środowiska produkcyjnego)
+        phrase = "rower"  # Przykładowa fraza wyszukiwania
+
+        #Użycie bloku try z powodu przewidzianego błędu nieaktualnego tokenu
+        #("requests.exceptions.HTTPError: 401 Client Error: Unauthorized for url:")
         try:
-            brand = product['parameters'][0]['valuesLabels'][0]
-            print(f"Marka: {brand}")
+            formatted_data = get_and_format_products(phrase, token)
         except:
-            #if brand:
-            #print("Producent:", brand.get('name', 'Brak danych'))
-            #else:
-            print("Producent: Brak danych")
+            token = get_access_token(CLIENT_ID,
+                                     CLIENT_SECRET)  # Uzyskaj token działa przez 0,5 h. Nie powinno się go co chwila odnawiać
+            formatted_data = get_and_format_products(phrase, token)
 
-        # Pobieramy inne szczegóły, takie jak EAN, jeśli są dostępne
-        print("EAN:", product.get('ean', 'Brak danych'))
-        print("Kategoria:", product.get('category', {}).get('name', 'Brak danych'))
-
-        # Separator dla czytelności
-        print("-" * 40)
+        return formatted_data
 
 
-# Użycie funkcji (podaj poprawne client_id i client_secret dla środowiska produkcyjnego)
-phrase = "rower"  # Przykładowa fraza wyszukiwania
-try:
-    get_products(phrase, CLIENT_ID, CLIENT_SECRET)
-except:
-    token = get_access_token(CLIENT_ID,
-                             CLIENT_SECRET)  # Uzyskaj token działa przez 0,5 h. Nie powinno się go co chwila odnawiać
-    get_products(phrase, CLIENT_ID, CLIENT_SECRET)
+
+
+    formatted_data = data_downloading_and_formatting()
+
+    return formatted_data
