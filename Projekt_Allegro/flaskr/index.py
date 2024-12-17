556732981d8ba7660c32222fa95256795fba72b7
jerrychutz <jeremiasz@chudzik.waw.pl>
2024-12-17 01:41:39 +0100

    Bardziej rozwojowa wersja wyświetlania danych za pomocą Flask
    Rozłączenie main na index i main, gdzie na ten moment main ma na celu pełnić rolę zwyczajowego __init__. Dodany folder na templatey.
    Rozwinięcie do struktury bliższej docelowej.
    
    Z najważniejszych zmian poza wymienionymi: linijki w index pozwalające traktować main jako widok, w main umieszczenie danych w słownikach dla uproszczenia (w następnych krokach będziemy się starali używać obiektów) dodany template, który zawiera ify i pętle, zmiana nazw funkcji i zmiennych zgodnie ze zmianą w procesach, wprowadzenie stałych zamiast zmiennych dla delay i max_products, które nie zmieniają się w trakcie działania kodu (refaktoring).

--- Projekt_Allegro/flaskr/index.py
+++ Projekt_Allegro/flaskr/index.py
@@ -1,77 +1,70 @@
 import requests
 from dotenv import load_dotenv
 import os
-from flask import Flask
 import time
+from flask import render_template
 
-app = Flask(__name__)
+# stała delay wskazuje ile sekund jest przerwy między kolejnymi połączeniami
+DELAY = 0
+# stała max_products wskazuje ile produktów chcemy pokazać w wynikach
+# 2024-12-16 20:48:56 do stałej max_products musi zostać przypisana wartość wielokrotności 30
+# inaczej rzeczywista liczba wyników przekroczy wartość max_products
+MAX_PRODUCTS = 210
 
-
-@app.route("/")
 def data_display():
-    def data_downloading_and_formatting():
 
-        def get_and_format_products(phrase, token, delay=0, max_products=200):
-            #zmienna delay wskazuje ile sekund jest przerwy między kolejnymi połączeniami
-            #zmienna max_products wskazuje ile produktów chcemy pokazać w wynikach
+    def data_download_and_preparation():
+
+        def get_and_process_products_data(phrase, token):
             PRODUCTS_URL = "https://api.allegro.pl/sale/products"
 
-            # Parametry zapytania i nagłówki
             headers = {
                 'Authorization': f'Bearer {token}',
                 'Accept': 'application/vnd.allegro.public.v1+json',
             }
             # id kolejnej strony generowany przez API, dzięki temu mamy dostęp do kolejnego zestawu 30 produktów
             next_page_id = ''
-            #Tablica wszystkich produktów
             all_products = []
-            #Ilość wszystkich produktów
             number_products = 0
-            while number_products <= max_products:
+
+            while number_products < MAX_PRODUCTS:
 
                 params = {
                     'phrase': phrase,
                     'page.id': next_page_id
                 }
 
-                # Pobieranie produktów
                 response = requests.get(PRODUCTS_URL, headers=headers, params=params)
                 response.raise_for_status()
                 data = response.json()
-
-                # pobiera id kolejnej strony
                 next_page_id = data['nextPage']['id']
-
-                # Przetwarzanie wyników
                 products = data.get('products', [])
-
-                #stworzenie tablicy wszystkich produktów
                 all_products.extend(products)
-                #liczba wszystkich produktów
+
                 number_products += len(products)
-                time.sleep(delay)
-            # Wyświetlenie wyników
-            formatted_data = f"Liczba produktów dla frazy '{phrase}': {len(all_products)}"
-            for product in all_products:
 
-                formatted_data = formatted_data + "<br>" + f"Produkt ID: {product.get('id', 'Brak danych')}"
-                formatted_data = formatted_data + "<br>" + f"Nazwa: {product.get('name', 'Brak nazwy')}"
+                time.sleep(DELAY)
 
-                # Pobieramy nazwę producenta, jeśli jest dostępna
+
+            # Porządkowanie wyników
+            data_allegro_products = {'phrase': phrase}
+            data_allegro_products['amount'] = len(all_products)
+            for ordinal, product in enumerate(all_products):
+
+                data_allegro_product = {'ID Produktu': product.get('id', 'Brak danych')}
+                data_allegro_product['Nazwa'] = product.get('name', 'Brak nazwy')
+
                 try:
                     brand = product['parameters'][0]['valuesLabels'][0]
-                    formatted_data = formatted_data + "<br>" + f"Marka: {brand}"
+                    data_allegro_product['Marka'] = brand
                 except:
-                    formatted_data = formatted_data + "<br>" + "Producent: Brak danych"
+                    data_allegro_product['Marka'] = "Producent: Brak danych"
 
-                # Pobieramy inne szczegóły, takie jak EAN, jeśli są dostępne
-                formatted_data = formatted_data + "<br>" + f"EAN: {product.get('ean', 'Brak danych')}"
-                # Nowa linia + separator dla czytelności
-                formatted_data = formatted_data + "<br>" + "-" * 40
+                data_allegro_products[f'Product{ordinal}'] = data_allegro_product
 
-            formatted_data = f"<p>{formatted_data}</p>"
 
-            return formatted_data
+            return data_allegro_products
+
 
         def get_access_token(client_id, client_secret):
             """
@@ -90,6 +83,7 @@ def data_display():
 
             return token
 
+
         load_dotenv()
         # Dane do uwierzytelnienia
         #zmienne lokalne znajdują się w pliku .env
@@ -102,7 +96,6 @@ def data_display():
         PRODUCTS_URL = "https://api.allegro.pl/sale/products"
         file_token_path = "./token.txt"
 
-        # Jeżeli plik token.txt istnieje. Pobiera się token z pliku.
         if os.path.exists(file_token_path) == False:
             file_token = open(f"{file_token_path}", "w")
             file_token.close()
@@ -111,24 +104,20 @@ def data_display():
             with open(f"{file_token_path}", "r") as file_token:
                 token = file_token.readline()
 
-        # Użycie funkcji (podaj poprawne client_id i client_secret dla środowiska produkcyjnego)
         phrase = "rower"  # Przykładowa fraza wyszukiwania
 
         #Użycie bloku try z powodu przewidzianego błędu nieaktualnego tokenu
         #("requests.exceptions.HTTPError: 401 Client Error: Unauthorized for url:")
         try:
-            formatted_data = get_and_format_products(phrase, token)
+            pack_data_allegro_products = get_and_process_products_data(phrase, token)
         except:
             token = get_access_token(CLIENT_ID,
-                                     CLIENT_SECRET)  # Uzyskaj token działa przez 0,5 h. Nie powinno się go co chwila odnawiać
-            formatted_data = get_and_format_products(phrase, token)
+                                     CLIENT_SECRET)  # Uzyskany token działa przez 0,5 h. Unikajmy odnawiania
+            pack_data_allegro_products = get_and_process_products_data(phrase, token)
 
-        return formatted_data
-
-    formatted_data = data_downloading_and_formatting()
-
-    return formatted_data
+        return pack_data_allegro_products
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    pack_data_allegro_products = data_download_and_preparation()
+
+    return render_template('index.html', data = pack_data_allegro_products)
