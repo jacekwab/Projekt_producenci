55cbfc0adcbec9c77ff975f9b084c1824389ff50
jerrychutz <jeremiasz@chudzik.waw.pl>
2025-01-17 15:49:51 +0100

    Poprawienie kierowania do urli i do funkcji. (#8)
    Rozdzielenie data_display na 2 funkcje.
    Zmiany w main wprowadzające oddzielne funkcje do 2 zasad.
    
    Poprawienie linków odsyłających z powrotem do naszej strony głównej (formularza wyszukiwania). Żaden z nich teraz nie crashuje.

--- Projekt_Allegro/flaskr/index.py
+++ Projekt_Allegro/flaskr/index.py
@@ -11,117 +11,113 @@ DELAY = 0
 # inaczej rzeczywista liczba wyników przekroczy wartość max_products
 MAX_PRODUCTS = 210
 
+def search_form_display():
+    return render_template('main.html')
 
 def data_display():
-    if request.method == "GET":
-        #ładuje formularz do wpisania hasła
-        return render_template('main.html')
-    else:
-        if 'phrase' in request.form:
-            # pobiera hasło z formularza
-            phrase = request.form['phrase']
-            if phrase.strip() == "": # Sprawdź, czy phrase jest pusty lub zawiera tylko białe znaki
-                return redirect(url_for('data_display'))
+    phrase = request.form['phrase']
+    if phrase.strip() == "": # Sprawdź, czy phrase jest pusty lub zawiera tylko białe znaki
+        return redirect(url_for('search_form_display'))
 
-        def data_download_and_preparation():
+    def data_download_and_preparation():
 
-            def get_and_process_products_data(phrase, token):
-                PRODUCTS_URL = "https://api.allegro.pl/sale/products"
-
-                headers = {
-                    'Authorization': f'Bearer {token}',
-                    'Accept': 'application/vnd.allegro.public.v1+json',
-                }
-                # id kolejnej strony generowany przez API, dzięki temu mamy dostęp do kolejnego zestawu 30 produktów
-                next_page_id = ''
-                all_products = []
-                number_products = 0
-
-                while number_products < MAX_PRODUCTS:
-                    params = {
-                        'phrase': phrase,
-                        'page.id': next_page_id
-                    }
-
-                    response = requests.get(PRODUCTS_URL, headers=headers, params=params)
-                    response.raise_for_status()
-                    data = response.json()
-                    products = data.get('products', [])
-                    all_products.extend(products)
-
-                    number_products += len(products)
-                    next_page = data.get('nextPage')
-                    if not next_page: # Brak kolejnej strony
-                        break
-                    next_page_id = data['nextPage']['id']
-                    time.sleep(DELAY)
-
-                # Porządkowanie wyników
-                data_allegro_products = {'phrase': phrase}
-                data_allegro_products['amount'] = len(all_products)
-                for ordinal, product in enumerate(all_products):
-
-                    data_allegro_product = {'ID Produktu': product.get('id', 'Brak danych')}
-                    data_allegro_product['Nazwa'] = product.get('name', 'Brak nazwy')
-
-                    try:
-                        brand = product['parameters'][0]['valuesLabels'][0]
-                        data_allegro_product['Marka'] = brand
-                    except:
-                        data_allegro_product['Marka'] = "Producent: Brak danych"
-
-                    data_allegro_products[f'Product{ordinal}'] = data_allegro_product
-
-                return data_allegro_products
-
-            def get_access_token(client_id, client_secret):
-                """
-                Funkcja uzyskuje token dostępu do API Allegro.
-                """
-                response = requests.post(
-                    AUTH_URL,
-                    auth=(client_id, client_secret),
-                    data={'grant_type': 'client_credentials'}
-                )
-                response.raise_for_status()
-                token = response.json()['access_token']
-
-                with open(f"{file_token_path}", "w") as file_token:
-                    file_token.write(token)
-
-                return token
-
-            load_dotenv()
-            # Dane do uwierzytelnienia
-            #zmienne lokalne znajdują się w pliku .env
-            CLIENT_ID = os.getenv("CLIENT_ID")
-            CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
-            # Endpointy Allegro API
-            AUTH_URL = "https://allegro.pl/auth/oauth/token"
-            SEARCH_URL = "https://api.allegro.pl/offers/listing"
+        def get_and_process_products_data(phrase, token):
             PRODUCTS_URL = "https://api.allegro.pl/sale/products"
-            file_token_path = "./token.txt"
 
-            if os.path.exists(file_token_path) == False:
-                file_token = open(f"{file_token_path}", "w")
-                file_token.close()
-                token = ""
-            else:
-                with open(f"{file_token_path}", "r") as file_token:
-                    token = file_token.readline()
+            headers = {
+                'Authorization': f'Bearer {token}',
+                'Accept': 'application/vnd.allegro.public.v1+json',
+            }
+            # id kolejnej strony generowany przez API, dzięki temu mamy dostęp do kolejnego zestawu 30 produktów
+            next_page_id = ''
+            all_products = []
+            number_products = 0
 
-            #Użycie bloku try z powodu przewidzianego błędu nieaktualnego tokenu
-            #("requests.exceptions.HTTPError: 401 Client Error: Unauthorized for url:")
-            try:
-                pack_data_allegro_products = get_and_process_products_data(phrase, token)
-            except:
-                token = get_access_token(CLIENT_ID,
-                                         CLIENT_SECRET)  # Uzyskany token działa przez 0,5 h. Unikajmy odnawiania
-                pack_data_allegro_products = get_and_process_products_data(phrase, token)
+            while number_products < MAX_PRODUCTS:
+                params = {
+                    'phrase': phrase,
+                    'page.id': next_page_id
+                }
 
-            return pack_data_allegro_products
+                response = requests.get(PRODUCTS_URL, headers=headers, params=params)
+                response.raise_for_status()
+                data = response.json()
+                products = data.get('products', [])
+                all_products.extend(products)
 
-        pack_data_allegro_products = data_download_and_preparation()
+                number_products += len(products)
+                next_page = data.get('nextPage')
+                if not next_page: # Brak kolejnej strony
+                    break
+                next_page_id = data['nextPage']['id']
+                time.sleep(DELAY)
 
-        return render_template('results.html', data=pack_data_allegro_products)
+            # Porządkowanie wyników
+            data_allegro_products = {'phrase': phrase}
+            data_allegro_products['amount'] = len(all_products)
+            for ordinal, product in enumerate(all_products):
+
+                data_allegro_product = {'ID Produktu': product.get('id', 'Brak danych')}
+                data_allegro_product['Nazwa'] = product.get('name', 'Brak nazwy')
+
+                try:
+                    brand = product['parameters'][0]['valuesLabels'][0]
+                    data_allegro_product['Marka'] = brand
+                except:
+                    data_allegro_product['Marka'] = "Producent: Brak danych"
+
+                data_allegro_products[f'Product{ordinal}'] = data_allegro_product
+
+            return data_allegro_products
+
+        def get_access_token(client_id, client_secret):
+            """
+            Funkcja uzyskuje token dostępu do API Allegro.
+            """
+            response = requests.post(
+                AUTH_URL,
+                auth=(client_id, client_secret),
+                data={'grant_type': 'client_credentials'}
+            )
+            response.raise_for_status()
+            token = response.json()['access_token']
+
+            with open(f"{file_token_path}", "w") as file_token:
+                file_token.write(token)
+
+            return token
+
+        load_dotenv()
+        # Dane do uwierzytelnienia
+        #zmienne lokalne znajdują się w pliku .env
+        CLIENT_ID = os.getenv("CLIENT_ID")
+        CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+        # Endpointy Allegro API
+        AUTH_URL = "https://allegro.pl/auth/oauth/token"
+        SEARCH_URL = "https://api.allegro.pl/offers/listing"
+        PRODUCTS_URL = "https://api.allegro.pl/sale/products"
+        file_token_path = "./token.txt"
+
+        if os.path.exists(file_token_path) == False:
+            file_token = open(f"{file_token_path}", "w")
+            file_token.close()
+            token = ""
+        else:
+            with open(f"{file_token_path}", "r") as file_token:
+                token = file_token.readline()
+
+        #Użycie bloku try z powodu przewidzianego błędu nieaktualnego tokenu
+        #("requests.exceptions.HTTPError: 401 Client Error: Unauthorized for url:")
+        try:
+            pack_data_allegro_products = get_and_process_products_data(phrase, token)
+        except:
+            token = get_access_token(CLIENT_ID,
+                                     CLIENT_SECRET)  # Uzyskany token działa przez 0,5 h. Unikajmy odnawiania
+            pack_data_allegro_products = get_and_process_products_data(phrase, token)
+
+        return pack_data_allegro_products
+
+    pack_data_allegro_products = data_download_and_preparation()
+
+    return render_template('results.html', data=pack_data_allegro_products)
--- Projekt_Allegro/flaskr/main.py
+++ Projekt_Allegro/flaskr/main.py
@@ -3,12 +3,11 @@ from flask import Flask
 def create_app():
 
     app = Flask(__name__)
-    from index import data_display
-    app.add_url_rule('/results', endpoint = None, view_func = data_display, methods=["GET", "POST"])
-    app.add_url_rule('/', endpoint=None, view_func=data_display, methods=["GET", "POST"])
+    from index import data_display, search_form_display
+    app.add_url_rule('/', endpoint=None, view_func=search_form_display, methods=["GET"])
+    app.add_url_rule('/results', endpoint = None, view_func = data_display, methods=["POST"])
     return app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
