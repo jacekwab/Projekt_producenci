3389d651b22719db1728fa5ed559ed99471f4291
jacekwab <wabik.jacek@gmail.com>
2025-01-21 14:08:10 +0100

    Wersja z testami programu (#9)
    * Wersja programu z testami odpowiedzi, który sprawdza, za długie hasło (powyżej 2000 znaków), typowe hasło, hasło ze znamkami specjalnymi, wejście na stronę domową, brak przekierowania w przypadku wpisania pustego hasła
    
    * Usunięcie testu zbyt długiego słowa oraz poprawka pliku main.py, w którym import pliku index.py odnosi się do ścieżki pliku

--- Projekt_Allegro/flaskr/main.py
+++ Projekt_Allegro/flaskr/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
 
     app = Flask(__name__)
-    from index import data_display, search_form_display
+    from flaskr.index import data_display, search_form_display
     app.add_url_rule('/', endpoint=None, view_func=search_form_display, methods=["GET"])
     app.add_url_rule('/results', endpoint = None, view_func = data_display, methods=["POST"])
     return app
---
+++ Projekt_Allegro/flaskr/tests/test_index.py
@@ -0,0 +1,38 @@
+import pytest
+import sys
+import os
+# Add the parent directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from flaskr.main import app
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_home(client):
+    """Test the home route."""
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_data_display_post_valid_phrase(client):
+    """Test the /results route with a valid input."""
+    response = client.post('/results', data={'phrase': 'valid input'})
+    assert response.status_code == 200
+
+
+def test_data_display_post_empty_phrase(client):
+    rv = client.post('/', data={'phrase': '  '})
+    assert rv.status_code == 405  # Sprawdza, czy nie ma możliwości przekierowania
+    assert rv.location is None  # Sprawdza, czy nie ma przekierowania na żadną stronę
+
+
+def test_data_display_post_special_characters(client):
+    """Test the /results route with a phrase containing special characters."""
+    special_characters_phrase = '!@#$%^&*()_+{}:"<>?[];\',./`~'
+    response = client.post('/results', data={'phrase': special_characters_phrase})
+    assert response.status_code == 200
