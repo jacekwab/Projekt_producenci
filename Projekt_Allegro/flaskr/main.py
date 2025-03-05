d70e17f939e243581c1fc655b64516cc56e4e4a3
Jacek Wabik <wabik.jacek@gmail.com>
2025-03-05 18:25:29 +0100

    Wersja z inicjacją bazy danych użytkowników i ustaleniem admina

--- Projekt_Allegro/flaskr/main.py
+++ Projekt_Allegro/flaskr/main.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from error_logger import setup_global_exception_logging, init_db
-
+import time
+from flaskr.index import data_display, search_form_display, check_connection, db, init_user_db
 
 def create_app():
     app = Flask(__name__)
     # Inicjalizacja bazy danych i ustawienie globalnego logowania błędów
     init_db()
+    init_user_db(app)
     setup_global_exception_logging()
-    import time
-    from flaskr.index import data_display, search_form_display, check_connection
+
     CORS(app, resources={r"/check_connection": {"origins": "*"}})  # Allow all origins for testing
 
     app.add_url_rule('/check_connection', endpoint=None, view_func=check_connection, methods=['GET', 'POST'])
